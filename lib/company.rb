a5bcdc1717af4410996c7d96394a379a9aad235f
justinetroyke <jjtroyke@gmail.com>
2018-04-17 12:00:05 -0600

    merging

--- lib/company.rb
+++ lib/company.rb
@@ -1,11 +1,12 @@
 require_relative 'employee'
+require 'csv'
 
 class Company
 
   attr_reader :employees,
               :projects,
               :timesheets,
-              :success
+              :success_status
 
   def initialize
     @employees = []
@@ -16,11 +17,12 @@ class Company
   def load_employees(file_path)
     CSV.foreach(file_path) do |row|
       if row.length != 5
-        @success_status = {success: true, error: nil}
-      else
         @success_status = {success: false, error: 'bad data'}
+      else
+        @success_status = {success: true, error: nil}
       end
-       @ employees << Employee.new(row[0], row[1],
-   end
+      @employees << Employee.new(row[0], row[1], row[2], row[3], row[4])
+    end
+    @success_status
   end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -1,6 +1,6 @@
 require './test/test_helper'
 require './lib/company'
-require './lib/file_loader'
+require './lib/employee'
 
 class CompanyTest < Minitest::Test
   attr_reader :company
@@ -25,20 +25,21 @@ class CompanyTest < Minitest::Test
     assert_equal [], company.timesheets
   end
 
-  def test_it_can_load_employess_from_file
+  def test_it_can_upload_file
+    company.load_employees('./data/employees.csv')
+
+    assert_equal 'Susan Smith', company.employees[0].name
+  end
+
+  def test_it_can_show_successful_uploads_from_file
     expected = {success: true, error: nil}
+
     assert_equal expected, company.load_employees('./data/employees.csv')
   end
+
+  def test_it_can_show_failure_of_upload
+    expected = {success: false, error: 'bad data'}
+
+    assert_equal expected, company.load_employees('./data/bad_employees.csv')
+  end
 end
-
-
-# Parameters: String
-# Returns: Hash
-# Read all data from filename in CSV format. Only add its contents to company.employees if ALL data in the file is valid. If any data is invalid, you should immediately return an error INSTEAD of saving the data. A test file for bad employee data has been provided.
-#
-# This method should return a hash. A success key in the hash will contain a boolean letting us know if the whole file imported correctly or not. An error key, if success is false, will tell us why the import failed.
-#
-# Return value examples:
-#
-# {success: true, error: nil}
-# {success: false, error: 'bad data'}
