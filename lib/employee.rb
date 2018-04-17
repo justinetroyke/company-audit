3e839ffd1a375b0ea90845ed8356c6f3548a8020
justinetroyke <jjtroyke@gmail.com>
2018-04-17 09:27:28 -0600

    initial commit

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,15 @@
 class Employee
+  attr_reader :id,
+              :name,
+              :role,
+              :start_date,
+              :end_date
+
+  def initialize(employee_id, name, role, start_date, end_date)
+    @id = employee_id.to_i
+    @name = name
+    @role = role
+    @start_date = Date.parse(start_date)
+    @end_date = Date.parse(end_date)
+  end
 end
--- test/employee_test.rb
+++ test/employee_test.rb
@@ -2,9 +2,38 @@ require './test/test_helper'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
-  def test_instantiation
+  attr_reader :employee
+
+  def setup
+    @employee = Employee.new('5', 'Sally Jackson', 'Engineer', '2015-01-01', '2018-01-01')
   end
 
-  def test_attributes
+  def test_class_exists
+    assert_instance_of Employee, employee
+  end
+
+  def test_it_has_employee_id
+    assert_equal 5, employee.id
+    assert_equal Integer, employee.id.class
+  end
+
+  def test_it_has_employee_name
+    assert_equal 'Sally Jackson', employee.name
+  end
+
+  def test_it_has_employee_role
+    assert_equal 'Engineer', employee.role
+  end
+
+  def test_it_has_employee_start_date
+    expected = Date.parse('2015-01-01')
+
+    assert_equal expected, employee.start_date
+  end
+
+  def test_it_has_employee_end_date
+    expected = Date.parse('2018-01-01')
+
+    assert_equal expected, employee.end_date
   end
 end
